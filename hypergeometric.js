a32482a62b76dc592bb3e96cc60546ac6240b569
Ryan1729 <Ryan1729@gmail.com>
2024-10-11 01:06:00 -0600

    add slow version of uniqueDraws calculation

--- hypergeometric.js
+++ hypergeometric.js
@@ -35,10 +35,42 @@ var Hypergeometric = (function () {
         return Number(PMF_SCALE_UP * (choose(K, k) * choose(N - K, n - k)) / choose(N, n)) / PMF_SCALE_DOWN;
     };
 
+    const uniqueDraws = (classCounts, drawCount) => {
+        if (drawCount <= 0) {
+            return 1;
+        }
+
+        if (drawCount === 1) {
+            return classCounts.length;
+        }
+
+        // TODO Probably a faster way to do this, but this way seems clearly correct, so would work to benchmark a faster solution against
+        let total = 0;
+        for (let i = 0; i < classCounts.length; i += 1) {
+            const afterDraw = []
+            for (let j = 0; j < classCounts.length; j += 1) {
+                let count = classCounts[j];
+
+                if (i == j) {
+                    count -= 1;
+                }
+
+                if (count > 0) {
+                    afterDraw.push(count);
+                }
+            }
+
+            total += uniqueDraws(afterDraw, drawCount - 1);
+        }
+
+        return total;
+    };
+
     return {
         PMF_DIGITS,
         choose,
         factorial,
-        pmf
+        pmf,
+        uniqueDraws
     };
 }())
